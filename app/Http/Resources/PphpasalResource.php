8fcd906d1e40d5591617c5a867415a59ccce58c7
Saannrr <ihsan.ican2002@gmail.com>
2024-08-24 16:35:02 +0700

    add nomor bukti pemotongan di pph pasal resource

--- app/Http/Resources/PphpasalResource.php
+++ app/Http/Resources/PphpasalResource.php
@@ -15,6 +15,7 @@ class PphpasalResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'nomor_bukti_pemotongan' => 2000000000 + $this->id,  // Menggabungkan 2000000000 dengan id
             'id' => $this->id,
             'tahun_pajak' => $this->tahun_pajak,
             'masa_pajak' => $this->masa_pajak,
